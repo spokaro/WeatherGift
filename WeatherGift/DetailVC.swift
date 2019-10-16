92acbd747535d3e6bd0bb89f443552e4c335f20c
andres <spokaro21@gmail.com>
2019-10-16 14:06:05 -0400

    7.2 completed

--- WeatherGift/DetailVC.swift
+++ WeatherGift/DetailVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  DetailVC.swift
 //  WeatherGift
 //
 //  Created by Andrew Boucher on 10/16/19.
@@ -8,13 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class DetailVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
-
+    
 }
-
